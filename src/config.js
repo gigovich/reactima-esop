bf25a85431eeef2fa786baecd293190d80d5972a
Ilya Sidorov <1@ilya1.com>
2017-05-27 18:49:28 +0300

    build

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ export default {
     dateFormat: 'YY-MM-DD',
     //ethEndpoint: '/api/',
     // ethEndpoint: 'https://neufund.org/nodes/ropsten/',
-    // ethEndpoint: 'http://testrpc.nyusya.com:8545/',
-    ethEndpoint: 'https://ropsten.infura.io/',
+    ethEndpoint: 'http://testrpc.nyusya.com:8545/',
+    //ethEndpoint: 'https://ropsten.infura.io/',
     truffleArtifacts: '../../ESOP/deployed_artifacts/ropsten', // where ABIs and addresses of smart contracts are, used by webpack
     //truffleArtifacts: '../../ESOP/build/contracts',
 
