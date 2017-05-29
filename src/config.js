8949771053b3586ad0b4f574ef7fbc914d725aee
Ilya Sidorov <1@ilya1.com>
2017-05-29 20:21:24 +0300

    host change

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ export default {
     dateFormat: 'YY-MM-DD',
     //ethEndpoint: '/api/',
     // ethEndpoint: 'https://neufund.org/nodes/ropsten/',
-    ethEndpoint: 'http://localhost:8545/',
+    //ethEndpoint: 'http://localhost:8545/',
+    ethEndpoint: 'http://testrpc.nyusya.com:8545/',
     //ethEndpoint: 'https://ropsten.infura.io/',
     truffleArtifacts: '../../ESOP/deployed_artifacts/ropsten', // where ABIs and addresses of smart contracts are, used by webpack
     //truffleArtifacts: '../../ESOP/build/contracts',
